dd6b65004ac0db093218276cf2767bed17fcefde
Framos310 <75812108+Framos310@users.noreply.github.com>
2021-08-24 13:20:44 -0700

    Added repo info

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Where profile info will appear
 const overviewDiv = document.querySelector(".overview");
 const username = "Framos310"
+const repoList = document.querySelector(".repo-list")
 
 const getData = async function(){
     const res = await fetch(`https://api.github.com/users/${username}`);
@@ -23,6 +24,23 @@ const displayUserInfo = function(data){
         <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div> `
+    </div>`;
     overviewDiv.append(div)
+    fetchRepos()
+}
+
+const fetchRepos = async function(){
+    const gitRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`)
+    const data = await gitRepos.json()
+    //console.log(data)
+    displayRepoInfo(data)
+}
+
+const displayRepoInfo = function(repos){
+    for(const repo of repos){
+        const repoItem = document.createElement("li")
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 }
